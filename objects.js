48827288934689b66ff50c55c8c607396e077ad7
Kshitij Pandey <kshitijpandey25@gmail.com>
2024-05-27 20:16:41 +0530

    Json Api + object DeStructuring

--- objects.js
+++ objects.js
@@ -1,25 +1,40 @@
-//singleton
+// //singleton
 
-//object literals 
+// //object literals 
 
-const JsUser = {
-    name: "Kshitij",
-    age:24,
-    location:"India",
-    isLoggedIn : false
-}
+// const JsUser = {
+//     name: "Kshitij",
+//     age:24,
+//     location:"India",
+//     isLoggedIn : false
+// }
 
-// accessing elements of objects
+// // accessing elements of objects
 
-//  console.log(jsUser["name"])
+// //  console.log(jsUser["name"])
 
-//  jsUser.greeting = function(){
-//     console.log("Hello JS User")
+// //  jsUser.greeting = function(){
+// //     console.log("Hello JS User")
+// //  }
+// //  console.log(jsUser.greeting);
+
+
+//  JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
 //  }
-//  console.log(jsUser.greeting);
+//  console.log(JsUser.greetingTwo);
 
+const course ={
+    name : "Cohort",
+    price : 2999,
+    courseInstructor : "Harkiart"
+}
+const {courseInstructor : instructor}=course
 
- JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
- }
- console.log(JsUser.greetingTwo);
+console.log(instructor);
+
+[
+    {},
+    {},
+    {}
+]
