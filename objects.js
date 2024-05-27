d4089ee3d10773f86c0df436a929f926acb6b515
Kshitij Pandey <kshitijpandey25@gmail.com>
2024-05-27 19:15:26 +0530

    First commit

---
+++ objects.js
@@ -0,0 +1,25 @@
+//singleton
+
+//object literals 
+
+const JsUser = {
+    name: "Kshitij",
+    age:24,
+    location:"India",
+    isLoggedIn : false
+}
+
+// accessing elements of objects
+
+//  console.log(jsUser["name"])
+
+//  jsUser.greeting = function(){
+//     console.log("Hello JS User")
+//  }
+//  console.log(jsUser.greeting);
+
+
+ JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+ }
+ console.log(JsUser.greetingTwo);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+
+//  console.log(jsUser["name"])
+
+//  jsUser.greeting = function(){
+//     console.log("Hello JS User")
+//  }
+//  console.log(jsUser.greeting);
